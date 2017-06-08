c47db3bbde7310950bdaa70311c3b013b18b8bfb
Brian Dunn <brian@danceoftehheart.com>
2017-06-08 08:25:01 +0000

    Fix exclamation point deficit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "I must go down to the sea again, to the lonely sea and the sky..."
+    render html: "I must go down to the sea again, to the lonely sea and the sky!!!"
   end
 end
